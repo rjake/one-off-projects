0e5ea7919543b2866246a73acb2e32bb0b9d3598
Jake <rjake@sas.upenn.edu>
2023-03-01 23:40:45 -0500

    Use CSV instead of Rds file (#6)
    * Use top 100 packages
    
    * Deselect all others when "any" is selected
    
    * Revert how data is called
    
    * Adjust criteria for table inclusion (at least 3 col vs 5)
    
    * Attempt to view skimr with renderUI
    
    * Use CSV only & ready in libraries

--- R/find-good-datasets/find_datasets.R
+++ R/find-good-datasets/find_datasets.R
@@ -4,9 +4,39 @@ setwd(dirname(.rs.api.getSourceEditorContext()$path))
 library(tidyverse)
 library(glue)
 
+
+top_100 <- cranlogs::cran_top_downloads(when = "last-month", count = 100) # 3/1/2023
+
+use_packages <- c(
+  "datasets",
+  "AER",
+  "carData",
+  "dplyr",
+  "Ecdat",
+  "forcats",
+  "ggplot2",
+  "HistData",
+  "ISLR",
+  "lubridate",
+  "MASS",
+  "openintro",
+  "survival",
+  "tidyr",
+  "usdata",
+  "wooldridge"
+)
+
+# check that all are available
+if (!all(use_packages %in% .packages(all.available = TRUE))) {
+  setdiff(use_packages, .packages(all.available = TRUE)) |> 
+    glue_collapse("', '") |> 
+    glue(x = _, "these packages are missing: c('{x}')") |> 
+    stop()
+}
+
 base_data <-
-  data(package = .packages(all.available = TRUE))$results |>
-  as_tibble() |>
+  data(package = use_packages)$results |>
+  as_tibble() |> 
   rename_all(tolower) |>
   select(-libpath) |> 
   mutate(
@@ -14,10 +44,6 @@ base_data <-
     pkg_data = glue("{package}::{item}")
   ) |> 
   distinct() |> 
-  filter(
-    !str_detect(package, "^(headliner|maptools|plyr|simplecolors|sp)$"),
-    !str_detect(item, "^(coralPval|HHSCyberSecurityBreaches|Khan|NCI60|ipo|treering)$")
-  ) |> 
   mutate(
     origin = case_when(
       package == "datasets" ~ "base",
@@ -29,160 +55,124 @@ base_data <-
 
 
 
-
 get_data_info <- function(x) {
     max_rows <- 100
     max_cols <- 20
     
     pkg <- str_extract(x, ".*(?=::)")
     item <- str_extract(x, "(?<=::).*")
-    # if (pkg %in% c("AER", "Stat2Data")) {
-    #   data(list = item)
-    # } else {
-      data(list = item, package = pkg)
-    #}
-    df <- get(item)  #rlang::parse_expr(x) |> eval()
+    
+    # need this weird logic for data like AER::BankWages
+    data(list = item, package = pkg)
+    df <- get(item)
     rm(list = item, envir = globalenv())
-    df_class <- class(df)
+    
+    # metadata
+    item_class <- class(df)
     is_df <- inherits(df, "data.frame") & !inherits(df, "sf")
-    
-    
-    dim <- dim(df) |> paste(collapse = " x ")
+    dims <- dim(df) |> paste(collapse = " x ")
     is_trunc <- TRUE
     
-    # if (is.list(df) & !is_df) {
-    #   use_data <- map(df, head, max_rows / 2)
-    #   
-    # } else if (is.vector(df) & !is.list(df)) {
-    #   use_data <- head(df, max_rows)
-    #   dim <- length(df)
-    #   
-    # } else 
     if (!is_df) {
       return(NULL)
-      # use_data <- df
-      # is_trunc <- FALSE
-      
-    } else if (nrow(df) < max_rows & ncol(df) <= max_cols) {
-      use_data <- df
-      is_trunc <- FALSE
-      
-    } else {
-      use_data <- 
-        head(df, max_rows) |> 
-        mutate(across(where(is.character), as.factor)) |> 
-        mutate(across(where(is.factor), fct_drop)) |> 
-        select(1:min(ncol(df), max_cols)) # max of n columns
-      
-      attr(use_data, "row.names") <- row.names(use_data) |> head(max_rows)
-      attr(use_data, "na.action") <- NULL # can cause to be large size ex 
-      attr(use_data, "spec") <- NULL # comes in with some tibbles, ex openintro::hfi
-    }
+    } 
+    # else if (nrow(df) < max_rows & ncol(df) <= max_cols) {
+    #   use_data <- df
+    #   is_trunc <- FALSE
+    # } else {
+    #   use_data <- 
+    #     head(df, max_rows) |> 
+    #     mutate(across(where(is.character), as.factor)) |> 
+    #     mutate(across(where(is.factor), fct_drop)) |> 
+    #     select(1:min(ncol(df), max_cols)) # max of n columns
+    #   
+    #   attr(use_data, "row.names") <- row.names(use_data) |> head(max_rows)
+    #   attr(use_data, "na.action") <- NULL # can cause to be large size ex 
+    #   attr(use_data, "spec") <- NULL # comes in with some tibbles, ex openintro::hfi
+    # }
     
-    class_test <- function(df, class) {
-      sum(
-        sapply(
-          df,
-          function(col) {
-            class(col) |> discard(str_detect, "labelled") |> pluck(1) %in% class
-          }
-        )
-      )
-    }
-    
-    list(
+    class_types <- map_chr(df, ~pluck(class(.x), 1))
+
+    tibble(
       pkg_data = x,
-      data = use_data,
-      is_trunc = is_trunc,
-      dim = as.character(dim),
-      n_col = ncol(df),
+      pkg = pkg,
+      data = item,
+      #data = use_data,
+      #is_trunc = is_trunc,
+      dim = as.character(dims),
       n_row = nrow(df),
-      class = df_class[1],
-      n_discrete = class_test(df, c("character", "factor", "ordered")),
-      n_numeric = class_test(df, c("numeric", "integer", "labelled")),
-      n_date = class_test(df, c("Date", "POSIXct", "dttm"))
+      n_col = ncol(df),
+      class = item_class[1],
+      n_discrete = sum(class_types %in% c("character", "factor", "ordered")),
+      n_numeric = sum(class_types %in% c("numeric", "integer", "labelled")),
+      n_date = sum(class_types %in% c("Date", "POSIXct", "dttm")),
+      n_logic = sum(class_types %in% c("logical")),
+      n_other = n_col - n_discrete - n_numeric - n_date - n_logic
     )
 }
 
 
-get_data_info(x = "Stat2Data::BeeStings")
-get_data_info(x = "COUNT::fishing")
-get_data_info(x = "ks::platesf")
 get_data_info(x = "ggplot2::diamonds")
+get_data_info(x = "ggplot2::economics")
+get_data_info(x = "ggplot2::economics_long")
 get_data_info(x = "openintro::hfi")
-get_data_info(x = "stringr::sentences")
-get_data_info(x = "openintro::children_gender_stereo")
-get_data_info("datasets::Seatbelts")
+get_data_info(x = "AER::BankWages") # not actually available when called this way
+get_data_info(x = "openintro::antibiotics") # not truncated
+get_data_info(x = "openintro::ucla_f18") # has logic
+get_data_info(x = "stringr::sentences") # not df
+get_data_info(x = "datasets::Seatbelts") # not df
+
 
 ds_obj <-
-  map(
-    .x = base_data$pkg_data, #[201:205]
-    possibly(get_data_info, otherwise = NULL)
-  ) |>
-  set_names(base_data$pkg_data) |> 
-  discard(is.null)
+  map_dfr(
+    .x = base_data$pkg_data, 
+    .f = possibly(get_data_info, otherwise = NULL)
+  )
 
 
-# check sizes
-map(ds_obj, pluck, "data") |> 
-  map_df(object.size) |> 
-  gather() |> 
-  arrange(desc(value))
+# # check sizes
+# map(ds_obj, pluck, "data") |> 
+#   map_df(object.size) |> 
+#   gather() |> 
+#   arrange(desc(value))
 
-list_details <- 
-  ds_obj |> 
-  map( ~tidyselect:::select(.x, -data)) |> 
-  map_dfr(flatten)
+# sum(.Last.value$value)
+
+# list_details <- 
+#   ds_obj |> 
+#   map( ~tidyselect:::select(.x, -data)) |> 
+#   map_dfr(flatten)
 
 
-
-prep_details <-
-  list_details |> 
-  mutate(
-    category = case_when(
-      str_detect(class, "data.frame|nfnGroupedData|tbl_df") ~ "data.frame",
-      class %in% c("character", "factor", "numeric") ~ "vector",
-      str_detect(class, "ts$") ~ "ts",
-      TRUE ~ "other"
-    ),
-    is_df = category == "data.frame",
-    .after = pkg_data
-  ) |> 
-  mutate(
-    n_other = n_col - n_discrete - n_numeric - n_date
-  ) |>
-  print()
+# prep_details <-
+#   ds_obj |> 
+#   mutate(n_other = n_col - n_discrete - n_numeric - n_date - n_logic) |>
+#   print()
 
 
 data_details <- 
   base_data |> 
-  inner_join(prep_details) |> #count(package, sort = TRUE) |> 
   rename(
     pkg = package,
     name = item
   ) |> 
-  filter(
-    (n_row >= 30 & n_col >= 5)
-    | n_discrete > 0
-  ) |> 
+  inner_join(ds_obj) |> #count(package, sort = TRUE) |> 
   arrange(name) |> 
   print()
 
 
 data_details |> 
-  count(pkg)
+  count(pkg) |> 
+  print(n = Inf)
 
-data_details |> 
-#  filter(category == "other") |> 
-  count(category, class)
 
 write_csv(data_details, "data_details.csv")
 
-ds_list <- 
-  ds_obj[data_details$pkg_data] |> 
-  map(~tidyselect:::select(.x, data))
-  
-saveRDS(ds_list, "ds_list.Rds")
+# ds_list <- 
+#   ds_obj[data_details$pkg_data] |> 
+#   map(~tidyselect:::select(.x, data))
+#   
+# saveRDS(ds_list, "ds_list.Rds")
 #
 
 
