798854f188597531d33426bc0603bac7ce5dfbab
Jake <rjake@sas.upenn.edu>
2023-03-01 23:31:49 -0500

    Use CSV only & ready in libraries

--- R/find-good-datasets/find_datasets.R
+++ R/find-good-datasets/find_datasets.R
@@ -7,21 +7,35 @@ library(glue)
 
 top_100 <- cranlogs::cran_top_downloads(when = "last-month", count = 100) # 3/1/2023
 
-addl_pkgs <- c(
-  "AER", "carData", "Ecdat", "HistData", "ISLR", "MASS", 
-  "openintro", "survival", "usdata", "wooldridge"
+use_packages <- c(
+  "datasets",
+  "AER",
+  "carData",
+  "dplyr",
+  "Ecdat",
+  "forcats",
+  "ggplot2",
+  "HistData",
+  "ISLR",
+  "lubridate",
+  "MASS",
+  "openintro",
+  "survival",
+  "tidyr",
+  "usdata",
+  "wooldridge"
 )
 
 # check that all are available
-if (!all(c(addl_pkgs, top_100$package) %in% .packages(all.available = TRUE))) {
-  setdiff(c(addl_pkgs, top_100$package), .packages(all.available = TRUE)) |> 
+if (!all(use_packages %in% .packages(all.available = TRUE))) {
+  setdiff(use_packages, .packages(all.available = TRUE)) |> 
     glue_collapse("', '") |> 
     glue(x = _, "these packages are missing: c('{x}')") |> 
     stop()
 }
 
 base_data <-
-  data(package = c(top_100$package, addl_pkgs, "datasets"))$results |>
+  data(package = use_packages)$results |>
   as_tibble() |> 
   rename_all(tolower) |>
   select(-libpath) |> 
@@ -56,40 +70,44 @@ get_data_info <- function(x) {
     # metadata
     item_class <- class(df)
     is_df <- inherits(df, "data.frame") & !inherits(df, "sf")
-    dim <- dim(df) |> paste(collapse = " x ")
+    dims <- dim(df) |> paste(collapse = " x ")
     is_trunc <- TRUE
     
     if (!is_df) {
       return(NULL)
-    } else if (nrow(df) < max_rows & ncol(df) <= max_cols) {
-      use_data <- df
-      is_trunc <- FALSE
-    } else {
-      use_data <- 
-        head(df, max_rows) |> 
-        mutate(across(where(is.character), as.factor)) |> 
-        mutate(across(where(is.factor), fct_drop)) |> 
-        select(1:min(ncol(df), max_cols)) # max of n columns
-      
-      attr(use_data, "row.names") <- row.names(use_data) |> head(max_rows)
-      attr(use_data, "na.action") <- NULL # can cause to be large size ex 
-      attr(use_data, "spec") <- NULL # comes in with some tibbles, ex openintro::hfi
-    }
+    } 
+    # else if (nrow(df) < max_rows & ncol(df) <= max_cols) {
+    #   use_data <- df
+    #   is_trunc <- FALSE
+    # } else {
+    #   use_data <- 
+    #     head(df, max_rows) |> 
+    #     mutate(across(where(is.character), as.factor)) |> 
+    #     mutate(across(where(is.factor), fct_drop)) |> 
+    #     select(1:min(ncol(df), max_cols)) # max of n columns
+    #   
+    #   attr(use_data, "row.names") <- row.names(use_data) |> head(max_rows)
+    #   attr(use_data, "na.action") <- NULL # can cause to be large size ex 
+    #   attr(use_data, "spec") <- NULL # comes in with some tibbles, ex openintro::hfi
+    # }
     
     class_types <- map_chr(df, ~pluck(class(.x), 1))
 
-    list(
+    tibble(
       pkg_data = x,
-      data = use_data,
-      is_trunc = is_trunc,
-      dim = as.character(dim),
-      n_col = ncol(df),
+      pkg = pkg,
+      data = item,
+      #data = use_data,
+      #is_trunc = is_trunc,
+      dim = as.character(dims),
       n_row = nrow(df),
+      n_col = ncol(df),
       class = item_class[1],
       n_discrete = sum(class_types %in% c("character", "factor", "ordered")),
       n_numeric = sum(class_types %in% c("numeric", "integer", "labelled")),
       n_date = sum(class_types %in% c("Date", "POSIXct", "dttm")),
-      n_logic = sum(class_types %in% c("logical"))
+      n_logic = sum(class_types %in% c("logical")),
+      n_other = n_col - n_discrete - n_numeric - n_date - n_logic
     )
 }
 
@@ -106,40 +124,39 @@ get_data_info(x = "datasets::Seatbelts") # not df
 
 
 ds_obj <-
-  set_names(base_data$pkg_data) |> 
-  # head() |> 
-  map(.f = possibly(get_data_info, otherwise = NULL)) |>
-  discard(is.null)
+  map_dfr(
+    .x = base_data$pkg_data, 
+    .f = possibly(get_data_info, otherwise = NULL)
+  )
 
 
-# check sizes
-map(ds_obj, pluck, "data") |> 
-  map_df(object.size) |> 
-  gather() |> 
-  arrange(desc(value))
+# # check sizes
+# map(ds_obj, pluck, "data") |> 
+#   map_df(object.size) |> 
+#   gather() |> 
+#   arrange(desc(value))
 
-sum(.Last.value$value)
+# sum(.Last.value$value)
 
-list_details <- 
-  ds_obj |> 
-  map( ~tidyselect:::select(.x, -data)) |> 
-  map_dfr(flatten)
+# list_details <- 
+#   ds_obj |> 
+#   map( ~tidyselect:::select(.x, -data)) |> 
+#   map_dfr(flatten)
 
 
-prep_details <-
-  list_details |> 
-  mutate(n_other = n_col - n_discrete - n_numeric - n_date - n_logic) |>
-  print()
+# prep_details <-
+#   ds_obj |> 
+#   mutate(n_other = n_col - n_discrete - n_numeric - n_date - n_logic) |>
+#   print()
 
 
 data_details <- 
   base_data |> 
-  inner_join(prep_details) |> #count(package, sort = TRUE) |> 
   rename(
     pkg = package,
     name = item
   ) |> 
-  filter((n_row >= 30 & n_col >= 3) | n_discrete > 0) |> 
+  inner_join(ds_obj) |> #count(package, sort = TRUE) |> 
   arrange(name) |> 
   print()
 
@@ -151,11 +168,11 @@ data_details |>
 
 write_csv(data_details, "data_details.csv")
 
-ds_list <- 
-  ds_obj[data_details$pkg_data] |> 
-  map(~tidyselect:::select(.x, data))
-  
-saveRDS(ds_list, "ds_list.Rds")
+# ds_list <- 
+#   ds_obj[data_details$pkg_data] |> 
+#   map(~tidyselect:::select(.x, data))
+#   
+# saveRDS(ds_list, "ds_list.Rds")
 #
 
 
