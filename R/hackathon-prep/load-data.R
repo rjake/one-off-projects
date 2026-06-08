c2eb0ddca38c8c010fb8ebb30d8c51d1078ac8d8
Jake <rjake@sas.upenn.edu>
2026-06-08 19:33:02 -0400

    Update load-data.R

--- R/hackathon-prep/load-data.R
+++ R/hackathon-prep/load-data.R
@@ -43,7 +43,7 @@ library(tidyverse)
 library(glue)
 library(duckdb)
 
-use_dir <- tools::file_path_as_absolute("~/../Downloads/sj-hackathon")
+use_dir <- tools::file_path_as_absolute("input")
 db_name <- file.path(use_dir, "hackathon-files.duckdb")
 
 con <-
@@ -149,7 +149,7 @@ walk(
 )
 
 # update_cols ----
-infa_col_types <- 
+info_col_types <- 
   dbGetQuery(
     con, 
     "select 
@@ -176,7 +176,7 @@ likely_numeric <-
   )
 
 cols_to_update <- 
-  infa_col_types |> 
+  info_col_types |> 
   filter(data_type == "VARCHAR") |> 
   #arrange(column_name) |> 
   filter(
@@ -229,12 +229,97 @@ dbGetQuery(# look at an example
   print(n = Inf)
 
 # all columns ----
-infa_cols <- dbGetQuery(con, "select * from information_schema.columns")
+info_cols <- dbGetQuery(con, "select * from information_schema.columns")
 
-infa_cols |> 
+info_cols |> 
   select(table_name, column_name, data_type) |> 
   as_tibble()
 
 # ---- disconnect ----
 duckdb::dbDisconnect(con, shutdown = TRUE)
 
+
+
+
+
+
+
+
+dbListFields(con, table_name)
+dbListTables(con)
+
+
+
+
+
+
+
+file_cols_list <-
+  map(
+    file_paths,
+    ~{
+      df <- read_n_lines(.x, 100)
+      tibble(
+        col_name = names(df),
+        class = sapply(df, class) |> map_chr(pluck, 1)
+      )
+    }
+  )
+
+file_cols <- 
+  list_rbind(
+    file_cols_list,
+    names_to = "file"
+  )
+
+
+column_overlap <- 
+  file_cols |> 
+  group_by(col_name) |> 
+  summarise(
+    n_table = n(),
+    n_class = n_distinct(class),
+    classes = paste(sort(unique(class)), collapse = ", ")
+  )
+    
+
+
+
+read_and_assign <- function(file_name, n = 50) {
+  obj_name <- 
+    basename(file_name) |> 
+    tools::file_path_sans_ext() |> 
+    janitor::make_clean_names()
+  
+  if (exists(obj_name, envir = globalenv()) && !overwrite) {
+    print("already exists")
+    return(invisible())
+  }
+  
+  df <- 
+    read_delim(file_name, delim = "|", n_max = n) |> 
+    as_tibble() |> 
+    janitor::clean_names()
+
+  assign(
+    x = obj_name, 
+    value = df, 
+    envir = env
+  )
+}
+
+
+
+walk(
+  file_paths, 
+  ~read_and_assign(.x)
+)
+
+
+duckdb_read_csv(
+  conn = con,
+  name = table_name,
+  files = file,
+  nrow.check = Inf,
+  delim = "|"
+)
