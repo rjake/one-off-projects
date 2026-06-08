18e014329c37a7fe5a2eee972297b0276f0f9f92
Jake <rjake@sas.upenn.edu>
2026-06-08 19:23:57 -0400

    Update get-calendar-data.R

--- R/climbing-calendar/get-calendar-data.R
+++ R/climbing-calendar/get-calendar-data.R
@@ -1,16 +1,23 @@
 library(ical)
+library(tidyverse)
+library(glue)
+
 options(lubridate.week.start = 1)
 
 ical_feed <- 
   list(
     d_c = "https://app.rockgympro.com/ical/public/0d6c7fa257084fd3b9628ca1759d88eb",
     d_f = "https://app.rockgympro.com/ical/public/47ed069d575c4005aa50ab205c49c1be",
+    d_i = "https://app.rockgympro.com/ical/public/85f8670aade1492b96d7d9b1f2c5d6e8",
     m_c = "https://app.rockgympro.com/ical/public/ff72f7c8859742ada90d47dfdf1bbb97",
     m_f = "https://app.rockgympro.com/ical/public/ddfa32c028e24859a68ef61cecda5ed9",
+    m_i = "https://app.rockgympro.com/ical/public/92a2707d31d4452381c51f6847cafe52",
     r_c = "https://app.rockgympro.com/ical/public/7fa510dd88f746378b4e0d79da753e70",
     r_f = "https://app.rockgympro.com/ical/public/7905f67632b8482b9d15bfd1b7beef09",
+    r_i = "https://app.rockgympro.com/ical/public/e63a50c2e6e8416c81939d00e2db5653",
     s_c = "https://app.rockgympro.com/ical/public/8a61329b24414b9e9f5313d64661c0c8",
-    s_f = "https://app.rockgympro.com/ical/public/9c2215701f70418ea1ab25009b361e50"
+    s_f = "https://app.rockgympro.com/ical/public/9c2215701f70418ea1ab25009b361e50",
+    s_i = "https://app.rockgympro.com/ical/public/01c72cecfa4a47e085fee5bfd8b2bca5"
   ) |> 
   set_names(
     ~str_replace_all(
@@ -22,6 +29,7 @@ ical_feed <-
         "^s" = "salvage_yard",
         "c$" = "climbing",
         "f$" = "fitness",
+        "i$" = "instruction",
         "_" = "."
       )
     )
@@ -80,23 +88,28 @@ of_interest <-
   filter(
     !str_detect(
       summary,
-      "free pt neck|guest hours|guided.*experience|parents|run club|sauna closure|teen climb|vinyasa|vin to yin|women|yoga"
-    ) |
-      str_detect(summary, "doom|acro")
+      paste0(
+        "free pt neck|guest hours|guided.*experience",
+        "|intro to (boulder|lead|rope)",
+        "|parents|run club|sauna closure",
+        "|teen climb|vinyasa|vin to yin|women|yoga"
+      )
+    ) #|
+    #str_detect(summary, "doom|acro")
   )
 
 
 prep_plot <- 
   of_interest |> 
-  filter(
-    month == "Oct"
-  ) |> 
+  # filter(
+  #   month == "Oct"
+  # ) |> 
   add_count(summary, name = "n_event") |> 
   complete(
     date = 
       seq.Date(
         floor_date(today(), "week") |> as.Date(), 
-        max(prep_plot$date), 
+        max(of_interest$date), 
         by = "day"
       ),
     fill = list(
@@ -116,7 +129,7 @@ prep_plot <-
     label = 
       glue::glue(
         t = str_remove(time, "^0"),
-        s = str_extract(summary, "([\\w&]+( |\\b)){1,3}") |> trimws() |> str_wrap(12),
+        s = str_extract(summary, "([\\w&]+( |\\b)){1,4}") |> trimws() |> str_wrap(12),
         "{t}\n{s}"
       ) |> 
       str_remove("^NA\nNA$"),
@@ -130,12 +143,28 @@ prep_plot |>
   slice(1) |> 
   t()
 
+activity_colors <- 
+  c(
+    "climbing" = "cyan3", 
+    "fitness" = "orange",
+    "instruction" = "pink"
+  )
+
+date_range <-
+  list(
+    min = today(), 
+    max = today() + 13
+  )
 
 prep_plot |>
   filter(
-    (hour < 9 | hour > 17) | day %in% c("Sat", "Sun")
-    # location == "durham",
-    # type == "climbing"
+    #month == format(today(), "%b"),
+    between(date, date_range$min, date_range$max)
+  ) |> 
+  filter(
+    #location == "durham",
+    #type == "climbing",
+    #(hour < 9 | hour > 17) | day %in% c("Sat", "Sun")
   ) |> 
   arrange(time) |> 
   mutate(
@@ -155,7 +184,7 @@ prep_plot |>
     expand = expansion(add = 0.5)
   ) +
   geom_text(
-    aes(x = "M", y = -0.25, label = glue("{day} {format(date, '%m/%d')}")),
+    aes(x = "D", y = -0.25, label = glue("{day} {format(date, '%m/%d')}")),
     hjust = 0
   ) +
   geom_label(
@@ -184,8 +213,72 @@ prep_plot |>
     panel.grid = element_blank(),
     strip.text = element_blank()
   ) +
-  scale_fill_manual(values = c("cyan3", "orange")) +
+  scale_fill_manual(values = activity_colors) +
   labs(
     x = NULL,
     y = NULL
   )
+
+
+prep_plot |> 
+  drop_na(summary) |> 
+  filter(
+    #month == format(today(), "%b"),
+    between(date, date_range$min, date_range$max)
+  ) |> 
+  filter(
+    #location == "durham" 
+    #type != "climbing",
+    (hour > 17) | day %in% c("Sat", "Sun")
+  ) |> 
+  transmute(
+    dow = format(date, "%a") |> str_sub(1,1),
+    label = label |> str_replace_all("\n", " ") |> fct_reorder(time, "min", .desc = TRUE),
+    f = location,
+    x = date,
+    y = label,
+    type,
+    n_event,
+    week
+  ) |> 
+  # distinct(y) |> arrange(y)
+  ggplot(
+    aes(x = x, y = y)
+  ) +
+  facet_grid(
+    rows = vars(f),
+    scales = "free_y",
+    space = "free"
+  ) +
+  geom_tile(
+    aes(
+      fill = type,
+      alpha = -n_event
+    ),
+    color = "black",
+    width = 1
+  ) +
+  geom_text(aes(label = dow), size = 3) +
+  geom_vline(
+    data = ~filter(.x, x == week),
+    aes(xintercept = x-0.5)
+  ) +
+  theme(
+    #axis.text.y = element_blank(),
+    axis.ticks = element_blank(),
+    legend.position = "none",
+    panel.background = element_rect("white", "grey80"),
+    panel.grid = element_blank(),
+    #strip.text = element_blank()
+  ) +
+  scale_x_date(
+    labels = ~format(.x, "%m/%d") |> str_remove_all("\\b0"),
+    date_breaks = "1 day",
+    expand = expansion()
+  ) +
+  scale_fill_manual(values = activity_colors) +
+  labs(
+    x = NULL,
+    y = NULL
+  )
+
